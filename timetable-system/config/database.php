d1eb29ad267ff928b3f6e4e5bd64d12d88240d71
SherifWafaei1 <sherif2300591@miuegypt.edu.eg>
2025-12-22 22:38:15 +0200

    Commit

--- timetable-system/config/database.php
+++ timetable-system/config/database.php
@@ -44,6 +44,12 @@ class Database {
                 if (!empty($parts['path'])) $this->db_name = ltrim($parts['path'], '/');
             }
         }
+
+        // If a port is provided and host is localhost, force TCP by using 127.0.0.1
+        // This avoids PDO trying to use a Unix socket which yields "No such file or directory" inside containers.
+        if (!empty($this->port) && ($this->host === 'localhost' || $this->host === '::1')) {
+            $this->host = '127.0.0.1';
+        }
     }
 
     /**
@@ -72,7 +78,15 @@ class Database {
             $dsn .= ";dbname=" . $this->db_name . ";charset=" . $this->charset;
             $this->conn = new PDO($dsn, $this->username, $this->password, $this->options);
         } catch(PDOException $exception) {
+            // Log full PDO message for debugging
             error_log("Connection error: " . $exception->getMessage());
+
+            // If the error indicates a missing socket, add a hint to check DB_HOST/DB_PORT
+            if (strpos($exception->getMessage(), 'No such file or directory') !== false || strpos($exception->getMessage(), "Can't connect to") !== false) {
+                error_log("DB connection hint: If your DB is remote, set DB_HOST to the remote host (not 'localhost').\n" .
+                          "If using a Render managed database, use the provided host and port and set DB_PORT=3306.\n" .
+                          "To force TCP on localhost, set DB_PORT and/or set DB_FORCE_TCP=true.\n");
+            }
             // In debug mode, surface the real PDO message to help with diagnosing on Render.
             $debug = getenv('APP_DEBUG');
             if ($debug === '1' || strtolower($debug) === 'true') {
